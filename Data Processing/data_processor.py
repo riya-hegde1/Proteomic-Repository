2a547e2350eb978ad325de2febf7cc9a15814706
riya-hegde1 <141675225+riya-hegde1@users.noreply.github.com>
2023-10-07 19:54:14 -0400

    editing code

--- Data Processing/data_processor.py
+++ Data Processing/data_processor.py
@@ -2,12 +2,15 @@ import pandas as pd
 path = 'Data Submissions (Responses).xlsx'
 df = pd.read_excel('Data Submissions (Responses).xlsx')
 col = 66
+array = [[0]]
 while col < 91:
     titles = pd.read_excel(path, usecols = chr(col)).to_numpy()
     i = 0
     while i < len(titles):
-        print(titles[i][0])
+        array[col-66][i] = titles[i][0]
         i += 1
     col += 1
 
-
+for x in array:
+    for a in x:
+        print(a)

f1b03b7189ad00ac775c4060f31566d59f5cd6e4
Riya Hegde <hegdefamily@Riyas-MacBook-Pro.local>
2023-10-09 19:54:45 -0400

    [code can now update complete data based on google form responses]

--- Data Processing/data_processor.py
+++ Data Processing/data_processor.py
@@ -1,16 +1,4 @@
 import pandas as pd
-path = 'Data Submissions (Responses).xlsx'
-df = pd.read_excel('Data Submissions (Responses).xlsx')
-col = 66
-array = [[0]]
-while col < 91:
-    titles = pd.read_excel(path, usecols = chr(col)).to_numpy()
-    i = 0
-    while i < len(titles):
-        array[col-66][i] = titles[i][0]
-        i += 1
-    col += 1
-
-for x in array:
-    for a in x:
-        print(a)
+path = 'Data Processing/Data Submissions (Responses).xlsx'
+df = pd.read_excel(path, header=None)
+df.to_markdown("complete-data.md")
